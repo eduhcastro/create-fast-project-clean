94796547188bd78c2f26c7d487379b1b6c5d61c3
Eduardo Castro <projecflex@gmail.com>
2021-08-03 12:15:31 -0300

    Arquivos inuteis deletados

--- src/services/CreateFileNamespaceService.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../controllers/CreateFileNamespaceController.ts" />
-namespace Validation {
-  export interface StringValidator {
-    isAcceptable(s: string): boolean;
-  }
-}
-
